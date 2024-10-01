3bec9cd3417ff79c0e4516b618d1e910fadab3f9
Hritish Mahajan <hritish.mahajan@gmail.com>
2024-10-02 02:53:44 +0400

    Add Github Pages Deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+	base: "/student-club-elections/",
 	plugins: [react()],
 	server: {
 		proxy: {
